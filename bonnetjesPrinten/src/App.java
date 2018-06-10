b793079812f8360ba30bfeea92637b66eb252eff
Boas Kalma <0943995@hr.nl>
2018-06-10 21:02:51 +0200

    bonnen met afbeeldingen

--- bonnetjesPrinten/src/App.java
+++ bonnetjesPrinten/src/App.java
@@ -3,6 +3,6 @@ public class App {
 
 
     public static void main(String[] args0) {
-        PrinterTest print = new PrinterTest("Boas", "Kalma", "ATM@39", "35");
+        PrinterClass print = new PrinterClass("Boas", "Kalma", "ATM@39", "35");
     }
 }
